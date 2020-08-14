92cbfe186b3bcad8ae5d1fb01b0b098a0a568c5e
Jp1200 <jprkr96@gmail.com>
2020-08-13 22:53:56 -0500

    init

---
+++ app.py
@@ -0,0 +1,68 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import requests
+import datetime
+from alpha_vantage.timeseries import TimeSeries
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+api_key = open('alpha.txt', 'r').read()
+# ts = TimeSeries(key=api_key.format(api_key), output_format='JSON')
+
+
+# def getStock(stock):
+stock = 'MSFT'
+rstocks = requests.get(
+    'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&outputsize=compact&apikey=api_key')
+result = rstocks.json()
+result = result['Time Series (1min)']
+
+
+df = pd.DataFrame(columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+
+
+for i, p in result.items():
+    date = datetime.datetime.strptime(i, '%Y-%m-%d  %H:%M:%S')
+    dataForAllTimes_row = [date, float(p['1. open']), float(p['2. high']), float(
+        p['3. low']), float(p['4. close']), float(p['5. volume'])]
+    df.loc[-1, :] = dataForAllTimes_row
+    df.index = df.index+1
+
+    # plt.plot(int(date), dataForAllTimes_row[3])
+
+df = df.sort_values('date')
+print(df['open'])
+fig = px.scatter(df['open'], size_max=100)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[
+            {'label': "Microsoft", 'value': 'MSFT'},
+            {'label': "Aamazon", 'value': 'AMZN'}
+        ],
+        value='MSFT'
+    ),
+    html.Div(id='dd-output-container'),
+
+    dcc.Graph(
+        id='dd-output-container',
+        figure=fig
+    )
+])
+
+
+@ app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('demo-dropdown', 'value')]
+)
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
